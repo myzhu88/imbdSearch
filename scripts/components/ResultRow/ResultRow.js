e05728469449d63fee5a904cd0898edf5a75f2bc
Mike Zhu <myzhu88@gmail.com>
2017-04-23 20:08:08 -0400

    Button added to search results page

--- scripts/components/ResultRow/ResultRow.js
+++ scripts/components/ResultRow/ResultRow.js
@@ -14,11 +14,11 @@ class ResultRow extends Component {
 		let imdbID = this.props.data.imdbID || '';
 		return(
 			<Row className='resultRow'>
-				<Col xsHidden sm={1}><div className='verticalAlign'>{searchIndex+'.'}</div></Col>
+				<Col xsHidden sm={1} className="column"><div className='verticalAlign'>{searchIndex+'.'}</div></Col>
 				<Col xs={12} sm={2} className="movieImgContainer">{<img src={imgSRC} className="movieImg"/>}</Col>
-				<Col xs={12} sm={5}><div className='verticalAlign'>{title}</div></Col>
-				<Col xs={12} sm={2}><div className='verticalAlign'>{releaseYear}</div></Col>
-				<Col xs={12} sm={2}><div className='verticalAlign'>button</div></Col>
+				<Col xs={12} sm={5} className="column"><div className='verticalAlign'>{title}</div></Col>
+				<Col xs={12} sm={2} className="column"><div className='verticalAlign'><strong>Release: </strong>{releaseYear}</div></Col>
+				<Col xs={12} sm={2} className="column"><div className='verticalAlign'><Button bsStyle="success" bsSize="small">View Details</Button></div></Col>
 			</Row>
 		);
 
